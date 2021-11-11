720851e91548ce915417d918ad5eaae9ad45cb92
thasup <thanachonfirst@hotmail.com>
2021-11-11 17:30:02 +0700

    Add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: './src/client/index.js',
+    devtool: 'source-map',
     module: {
         rules: [
             {
@@ -16,5 +18,11 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
 };
