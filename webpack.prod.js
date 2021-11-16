4ea2c327e8945094492ec1d7eb30277bfc59156f
thasup <thanachonfirst@hotmail.com>
2021-11-16 23:02:31 +0700

    Add modes

---
+++ webpack.prod.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
+};
