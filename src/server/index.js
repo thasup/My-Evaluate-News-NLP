06fba9bb9364b8aac8ff9b14c06aebfd02950ac1
thasup <thanachonfirst@hotmail.com>
2021-11-19 12:59:15 +0700

    Add sass loader

--- src/server/index.js
+++ src/server/index.js
@@ -9,7 +9,7 @@ app.use(express.static('dist'))
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    res.sendFile('/dist/index.html')
+    res.sendFile(path.resolve('dist/index.html'))
 })
 
 const port = 8080;
